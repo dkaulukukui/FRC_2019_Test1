a7e424b1cbf9f13e47a730dfb05f21fd52453998
Donald Kaulukukui <donald.kaulukukui@gmail.com>
2019-01-11 13:06:23 -1000

    first

---
+++ src/main/java/frc/robot/commands/LED_ON.java
@@ -0,0 +1,50 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class LED_ON extends Command {
+  public LED_ON() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(Robot.LEDs);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    Robot.LEDs.Red_LED.set(true);
+    Robot.LEDs.Yellow_LED.set(true);
+    Robot.LEDs.Green_LED.set(true);
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/LED_Subsystem.java
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+import frc.robot.commands.LED_OFF; //Added by DK
+
+/**
+ * Add your docs here.
+ */
+public class LED_Subsystem extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  public DigitalOutput Green_LED = new DigitalOutput(0);
+  public DigitalOutput Red_LED = new DigitalOutput(1);
+  public DigitalOutput Yellow_LED = new DigitalOutput(2);
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    setDefaultCommand(new LED_OFF());
+  }
+}
